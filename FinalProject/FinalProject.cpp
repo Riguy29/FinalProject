d6c9f2e4aedd37a0c9a43215c5e10d203342e506
Riguy29 <riguygb@gmail.com>
2023-03-25 18:49:14 -0500

    First Commit

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -5,7 +5,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    std::cout << "Please log in\n Enter your username:";
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
