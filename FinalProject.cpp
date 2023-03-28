5ddb812813ecd7cb1f97d9a98b40f0ddf04dd6b8
Amber <amo.olson@gmail.com>
2023-03-28 18:00:39 -0500

    test include

---
+++ FinalProject.cpp
@@ -0,0 +1,15 @@
+// FinalProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "this is a test";
+    cout << "Please log in\n Enter your username:";
+    
+}
+
