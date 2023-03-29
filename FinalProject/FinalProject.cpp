aae23dd14f5bea50a589cff153f887e3ec586958
Amber <amo.olson@gmail.com>
2023-03-28 19:13:01 -0500

    added linked list and test class
    the test class is there until another is created to test the linkedlist

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -1,9 +1,67 @@
-#include <iostream>
-using namespace std;
+
+#include "LibraryLinkedList.cpp"
+#include "Test.h"
+#include <string>
+#include <cstdlib>
+#include <iomanip>
+#include <fstream>
 
 int main()
 {
 
-    cout << "test";
-    
+	//creating objects of BookList for testing LinkedList functions
+	BookList* book1 = new BookList("Harry Potter", "J.K. Rowling", "2001", 1, 8909789098, " ", " ", 35.00);
+	BookList* book2 = new BookList("Bad Guys", "Blabey", "2011", 23, 9809876678, " ", " ", 10.00);
+	BookList* book3 = new BookList("Information System", "Earl H. McKinney", " Pearson", 2022, 0136567, " ", " ", 75.00);
+
+
+	LinkedList<BookList*> Books;
+
+	Books.insertItem(book1);
+	Books.insertItem(book2);
+	Books.insertItem(book3);
+	Books.printList();
+	cout << "delete book1" << endl;
+	Books.deleteItem(book1);
+
+	Books.printList();
+
+	/*
+	LinkedList<string> Books;
+	Books.insertItem("Harry Potter");
+	Books.insertItem("Information System");
+	Books.insertItem("Data Structures");
+	Books.insertItem("Throne of Glass");
+	Books.insertItem("Man called Otto");
+	Books.insertItem("");
+	Books.printList();
+	cout << endl;
+	cout << "size of the list is: ";
+	cout << Books.size() << endl;
+	cout << endl;
+
+	cout << "Delete item Harry Potter and Spiderwick" << endl;
+	Books.deleteItem("Harry Potter");
+	Books.deleteItem("Spiderwick");
+	Books.printList();
+	cout << endl;
+	cout << "size of the list is: ";
+	cout << Books.size() << endl;
+	cout << endl;
+
+	cout << "insert Bad Guys" << endl;
+	Books.insertItem("Bad Guys");
+	Books.printList();
+	cout << endl;
+
+	cout << "size of the list is: ";
+	cout << Books.size() << endl;
+	cout << endl;
+
+	cout << "get value at index 4 and 2" << endl;
+	cout << Books.getItem(4) << endl;
+	cout << Books.getItem(2) << endl;
+
+	return 0;
+	*/
 }
---
+++ FinalProject/LibraryLinkedList.h
@@ -0,0 +1,39 @@
+#ifndef LIBRARYLINKEDLIST_H
+#define LIBRATYLINKEDLIST_H
+
+#include <iostream>
+#include <cassert> //check if the parameters passed to a function are valid, to check the return value of a 
+//function or to check the array bounds among other things.
+
+using namespace std;
+
+//Definition of Node as a struct
+template<typename T> //generic datatype
+struct Node
+{
+	T data; //stores the data of a node
+	Node <T>* next; //points to the next node in the list
+};
+
+//Definition of class LinkList
+template<typename T>
+class LinkedList {
+private:
+	Node<T>* head;
+	Node<T>* newNode(const T& value);
+	int count;
+
+public:
+	LinkedList();
+	~LinkedList();
+	void insertItem(const T& value);
+	//void insertSorted(const T& value);
+	void deleteItem(const T& value);
+	T& getItem(int pos)const;
+	int size()const;
+	void printList()const;
+};
+
+
+
+#endif // !LIBRARYLINKEDLIST_H
