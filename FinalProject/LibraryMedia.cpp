da4f6d99e60753ab1fdc892e8772f4b25011d33c
Riguy29 <riguygb@gmail.com>
2023-04-04 18:48:16 -0500

    ToString library started

--- FinalProject/LibraryMedia.cpp
+++ FinalProject/LibraryMedia.cpp
@@ -2,4 +2,107 @@
 
 void LibraryMedia::ToString()
 {
+	cout << "Title: " << title;
+	cout << " Type: " << mediaType;
+	cout << " ISBN: " << ISBN;
+	cout << " Category: " << category;
+	cout << " Sub-Category: " << subCategory;
+	cout << " Edition: " << edition;
+	cout << " Inventory Amt: " << inventoryCount;
+	
+	//FIXME add code to print authors and publishers
+}
+
+LibraryMedia::LibraryMedia()
+{
+
+}
+
+void LibraryMedia::SetTitle(string newTitle)
+{
+}
+
+string LibraryMedia::GetTitle()
+{
+	return string();
+}
+
+void LibraryMedia::SetAuthors(LinkedList<string> authorList)
+{
+	authors = authorList;
+}
+
+LinkedList<string> LibraryMedia::GetAuthors()
+{
+	return authors;
+}
+
+void LibraryMedia::SetPublishers(LinkedList<Publisher> publisherList)
+{
+	publishers = publisherList;
+}
+
+LinkedList<Publisher> LibraryMedia::GetPublishers()
+{
+	return LinkedList<Publisher>();
+}
+
+void LibraryMedia::SetISBN(string newISBN)
+{
+	ISBN = newISBN;
+}
+
+string LibraryMedia::GetISBN()
+{
+	return ISBN;
+}
+
+void LibraryMedia::SetPrice(int newPrice)
+{
+	price = newPrice;
+}
+
+double LibraryMedia::GetPrice()
+{
+	return price;
+}
+
+void LibraryMedia::SetCategory(string newCategory)
+{
+	category = newCategory;
+}
+
+string LibraryMedia::GetCategory()
+{
+	return category;
+}
+
+void LibraryMedia::SetSubCategory(string newSubCategory)
+{
+	subCategory = newSubCategory;
+}
+
+string LibraryMedia::GetSubCategory()
+{
+	return subCategory;
+}
+
+void LibraryMedia::SetInventoryCount(int newCount)
+{
+	inventoryCount = newCount;
+}
+
+int LibraryMedia::GetInventoryCount()
+{
+	return inventoryCount;
+}
+
+void LibraryMedia::SetEdition(string newEdition)
+{
+	edition = newEdition;
+}
+
+string LibraryMedia::GetEdition()
+{
+	return edition;
 }
--- FinalProject/LibraryMedia.h
+++ FinalProject/LibraryMedia.h
@@ -7,14 +7,8 @@ using namespace std;
 class LibraryMedia
 {
 private:
-	string Title;
-	enum  mediaTypes
-	{
-		book,
-		newspaper,
-		conferenceJournal,
-		periodical
-	};
+	string title;
+
 	mediaTypes mediaType;
 	LinkedList<Publisher> publishers;
 	LinkedList<string> authors;
@@ -26,15 +20,40 @@ private:
 	int inventoryCount;
 
 public:
+	enum  mediaTypes
+	{
+		book,
+		newspaper,
+		conferenceJournal,
+		periodical
+	};
+
+	void SetTitle(string newTitle);
+	string GetTitle();
+	void SetMediaType(mediaTypes mediaType);
+	mediaTypes GetMediaType();
 	void SetAuthors(LinkedList<string> authorList);
+	LinkedList<string> GetAuthors();
 	void SetPublishers(LinkedList<Publisher> publisherList);
+	LinkedList<Publisher> GetPublishers();
 	void SetISBN(string newISBN);
 	string GetISBN();
 	void SetPrice(int newPrice);
 	double GetPrice();
+	void SetCategory(string newCategory);
+	string GetCategory();
+	void SetSubCategory(string newSubCategory);
+	string GetSubCategory();
 
+	void SetInventoryCount(int newCount);
+	int GetInventoryCount();
+
+	void SetEdition(string newEdition);
+	string GetEdition();
 
 	void ToString();
+
+	LibraryMedia();
 
 
 };
