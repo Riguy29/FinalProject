b5d03a280788895b1bc05d4cf8df4b73082cc3b4
Riguy29 <riguygb@gmail.com>
2023-05-07 12:56:39 -0500

    Comments

--- FinalProject/UserInfoAccessScreen.h
+++ FinalProject/UserInfoAccessScreen.h
@@ -2,6 +2,8 @@
 using namespace std;
 #include <string>
 #include "CurrentSessionInfo.h"
+/* This class is used for when users want to access and change information about their account
+And if admins want to find other users and change their information*/
 class UserInfoAccessScreen 
 {
 public:
